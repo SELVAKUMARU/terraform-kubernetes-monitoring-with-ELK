2ffd3a631243e018b5b54b99fb378c9ad49db7e9
selvakumar <selvakumaru176@gmail.com>
2024-08-20 18:07:34 +0530

    module of ELK

---
+++ main.tf
@@ -0,0 +1,222 @@
+provider "kubernetes" {
+  config_path = "~/.kube/server.conf"
+}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/server.conf"
+  }
+}
+
+#elasticsearch
+resource "helm_release" "elasticsearch" {
+
+  name = "elasticsearch"
+
+  repository = "https://helm.elastic.co"
+  chart      = "elasticsearch"
+  namespace  = var.namespace
+
+  set {
+    name  = "replicas"
+    value = var.elasticsearch_replicas
+  }
+
+  set {
+    name  = "minimumMasterNodes"
+    value = var.elasticsearch_minimumMasterNodes
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = var.elasticsearch_requests_cpu
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = var.elasticsearch_requests_memory
+  }
+
+  set {
+    name  = "resources.limits.cpu"
+    value = var.elasticsearch_limits_cpu
+  }
+
+  set {
+    name  = "resources.limits.memory"
+    value = var.elasticsearch_limits_memory
+  }
+
+  set {
+    name  = "volumeClaimTemplate.resources.requests.storage"
+    value = var.elasticsearch_storage
+  }
+
+  set {
+    name  = "service.type"
+    value = var.elasticsearch_service_type
+  }
+
+  set {
+    name  = "secret.password"
+    value = var.elasticsearch_password
+  }
+
+  values = [
+    file("./terraform-kubernetes-monitoring-with-ELK/configs/elasticsearch/values.yaml")
+  ]
+
+}
+
+#logstash
+
+resource "helm_release" "logstash" {
+
+  name = "logstash"
+  depends_on = [helm_release.elasticsearch]
+  repository = "https://helm.elastic.co"
+  chart      = "logstash"
+  namespace  = var.namespace
+
+  set {
+    name  = "replicas"
+    value = var.logstash_replicas
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = var.logstash_requests_cpu
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = var.logstash_requests_cpu
+  }
+
+  set {
+    name  = "resources.limits.cpu"
+    value = var.logstash_limits_cpu
+  }
+
+  set {
+    name  = "resources.limits.memory"
+    value = var.logstash_limits_memory
+  }
+
+  set {
+    name  = "volumeClaimTemplate.resources.requests.storage"
+    value = var.logstash_storage
+  }
+
+  values = [
+    file("./terraform-kubernetes-monitoring-with-ELK/configs/logstash/values.yaml")
+  ]
+
+}
+
+resource "helm_release" "filebeat" {
+
+  name = "filebeat"
+  depends_on = [helm_release.elasticsearch,helm_release.logstash]
+  repository = "https://helm.elastic.co"
+  chart      = "filebeat"
+  namespace  = var.namespace
+
+  set {
+    name  = "replicas"
+    value = var.filebeat_replicas
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = var.filebeat_requests_cpu
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = var.filebeat_requests_memory
+  }
+
+  set {
+    name  = "resources.limits.cpu"
+    value = var.filebeat_limits_cpu
+  }
+
+  set {
+    name  = "resources.limits.memory"
+    value = var.filebeat_limits_memory
+  }
+
+  values = [
+    file("./terraform-kubernetes-monitoring-with-ELK/configs/filebeat/values.yaml")
+  ]
+
+}
+
+resource "helm_release" "kibana" {
+
+  name = "kibana"
+  depends_on = [helm_release.elasticsearch,helm_release.logstash,helm_release.filebeat]
+  repository = "https://helm.elastic.co"
+  chart      = "kibana"
+  namespace  = var.namespace
+
+  set {
+    name  = "replicas"
+    value = var.kibana_replicas
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value =  var.kibana_requests_cpu
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = var.kibana_requests_memory
+  }
+
+  set {
+    name  = "resources.limits.cpu"
+    value = var.kibana_limits_cpu
+  }
+
+  set {
+    name  = "resources.limits.memory"
+    value = var.kibana_limits_memory
+  }
+
+  set {
+    name  = "service.type"
+    value = var.kibana_service_type
+  }
+
+  set {
+    name  = "ingress.enabled"
+    value = var.kibana_ingress_enabled
+  }
+
+  set {
+    name  = "ingress.ingressClassName"
+    value = var.kibana_ingressClassName
+  }
+
+  set {
+    name  = "ingress.hosts[0].host"
+    value = var.kibana_ingress_host
+  }
+
+  values = [
+    file("./terraform-kubernetes-monitoring-with-ELK/configs/kibana/values.yaml")
+  ]
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,174 @@
+variable "namespace" {
+    default = "elastic-monitoring"
+    description = "all the resources will deploy here"
+}
+
+## elasticsearch
+
+variable "elasticsearch_replicas" {
+  type    = number
+  default = 1
+}
+
+variable "elasticsearch_minimumMasterNodes" {
+  type    = number
+  default = 1
+}
+
+variable "elasticsearch_requests_cpu" {
+  type    = string
+  default = "100m"
+}
+
+variable "elasticsearch_requests_memory" {
+  type    = string
+  default = "512M"
+}
+
+variable "elasticsearch_limits_cpu" {
+  type    = string
+  default = "1000m"
+}
+
+variable "elasticsearch_limits_memory" {
+  type    = string
+  default = "1Gi"
+}
+
+variable "elasticsearch_storage" {
+  type    = string
+  default = "2Gi"
+}
+
+variable "elasticsearch_service_type" {
+  type    = string
+  default = "ClusterIP"
+}
+
+variable "elasticsearch_password" {
+  type    = string
+  default = "admin123"
+}
+
+#logstash
+
+variable "logstash_replicas" {
+  type    = number
+  default = 1
+}
+
+variable "logstash_requests_cpu" {
+  type    = string
+  default = "100m"
+}
+
+variable "logstash_requests_memory" {
+  type    = string
+  default = "1536Mi"
+}
+
+variable "logstash_limits_cpu" {
+  type    = string
+  default = "1000m"
+}
+
+variable "logstash_limits_memory" {
+  type    = string
+  default = "1536Mi"
+}
+
+variable "logstash_storage" {
+  type    = string
+  default = "2Gi"
+}
+
+# filebeat
+
+variable "filebeat_replicas" {
+  type    = number
+  default = 1
+}
+
+variable "filebeat_requests_cpu" {
+  type    = string
+  default = "100m"
+}
+
+variable "filebeat_requests_memory" {
+  type    = string
+  default = "100Mi"
+}
+
+variable "filebeat_limits_cpu" {
+  type    = string
+  default = "1000m"
+}
+
+variable "filebeat_limits_memory" {
+  type    = string
+  default = "200Mi"
+}
+
+#kibana
+
+variable "kibana_replicas" {
+  type    = number
+  default = 1
+}
+
+variable "kibana_requests_cpu" {
+  type    = string
+  default = "1000m"
+}
+
+variable "kibana_requests_memory" {
+  type    = string
+  default = "1Gi"
+}
+
+variable "kibana_limits_cpu" {
+  type    = string
+  default = "1000m"
+}
+
+variable "kibana_limits_memory" {
+  type    = string
+  default = "1Gi"
+}
+
+variable "kibana_service_type" {
+  type    = string
+  default = "NodePort"
+}
+
+variable "kibana_ingress_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "kibana_ingressClassName" {
+  type    = string
+  default = "nginx"
+}
+
+variable "kibana_ingress_host" {
+  type    = string
+  default = "kibana.francium.com"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
